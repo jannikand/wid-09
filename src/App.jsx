fe6a59367c44ab54ceb6f8a7f67d54bbb0d4a886
jannikand <128466450+jannikand@users.noreply.github.com>
2024-11-23 13:16:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <Typography variant="h3" gutterBottom>
+      Hello React + MUI
+    </Typography>
+  );
+}
+
+export default App;
